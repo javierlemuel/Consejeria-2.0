93747cd5835d4bbb0f5b0f4a0f641b169ae69102
juanmelendez28 <118770773+juanmelendez28@users.noreply.github.com>
2025-02-04 15:25:06 -0400

    Update database.php

--- new_template/student/config/database.php
+++ new_template/student/config/database.php
@@ -2,28 +2,6 @@
 
 require_once "config.php";
 
-//config/database.php
-// // config/database.php
-// $host = '136.145.24.81';
-// $username = 'estccom';
-// $password = 'Prog@840ccom';
-// $database = 'ConsejeriaCCOM';
-
-
-// $host = "localhost"; // XAMPP crea el localhost
-// $username = "root"; // Usuario de phpMyAdmin
-// $password = ""; //contrasena de phpMyAdmin
-// $database = "counseling_db";
-
-// $host = "136.145.29.193"; // XAMPP crea el localhost
-// $username = "emamarsa"; // Usuario de phpMyAdmin
-// $password = "ema84023"; //contrasena de phpMyAdmin
-// $database = "emamarsa_db"; 
-
-// $host = "136.145.29.193"; // XAMPP crea el localhost
-// $username = "natramri"; // Usuario de phpMyAdmin
-// $password = "nat84023"; //contrasena de phpMyAdmin
-// $database = "natramri_db";
 
 $conn = new mysqli($host, $username, $password, $database);
 
